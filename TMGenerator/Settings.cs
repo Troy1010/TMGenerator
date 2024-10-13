9976c83d389402ac4f8eac881cc05130e8413b78
Troy <troyswanson1010@gmail.com>
2024-10-13 11:53:02 -0500

    start

---
+++ TMGenerator/Settings.cs
@@ -0,0 +1,16 @@
+﻿using Mutagen.Bethesda.WPF.Reflection.Attributes;
+
+namespace TMGenerator;
+
+public class Settings
+{
+    [MaintainOrder]
+    
+    [SettingName("FeatureTMFriendlierFactions")]
+    [Tooltip("I'm tired of always running into dead enemies.")]
+    public bool FeatureTMFriendlierFactions = false;
+    
+    [SettingName("FeatureReduceAggressionForFCOM")]
+    [Tooltip("I'm tired of always running into dead enemies.")]
+    public bool FeatureReduceAggressionForFCOM = true;
+}
